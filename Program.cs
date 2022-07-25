d165eb32dc3b3243d5c315f223a136d92e587b59
Ayomide-22 <ayomideagboola777@gmail.com>
2022-07-25 16:15:16 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Git1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first github project");
+            Console.WriteLine("Hello world");
+        }
+    }
+}
